0000939b7bc65c71bb40a6486f052272a150de15
CountNick <meijer.nick1@gmail.com>
2020-03-13 17:40:34 +0100

    :sparkles: Server
    Server now running and serving yr needs

--- server/app.js
+++ server/app.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 const express = require('express')
 const bodyParser = require('body-parser')
-const axios = require('axios')
+const fetch = require('node-fetch')
 const path = require('path')
 const app = express()
 const port = process.env.PORT || 5000
@@ -16,8 +16,37 @@ app
     .use(express.static(path.join(__dirname, 'static')))
     .get('/', homeRoute)
 
-function homeRoute(req, res) {
-    res.send('Hello World!')
+async function getData(){
+    const query = 'tolkien'
+    const endpoint = "https://zoeken.oba.nl/api/v1/search/?q=";
+    const detail = "Default";
+    const url = `${endpoint}${query}&authorization=${process.env.API_KEY}&detaillevel=${detail}&output=json`;
+
+    const config = {
+        Authorization: `Bearer ${process.env.API_SECRET}`
+      };
+
+    const response = await fetch(url)
+
+    const text = await response.text()
+
+    const JSONData = await JSON.parse(text.trim())
+
+    
+
+    return JSONData
+
+}
+
+    
+async function homeRoute(req, res) {
+    const data = await getData()
+
+    console.log('ooooooooooooo: ', data.results)
+
+    res.render('home.hbs', {
+        books: data.results
+    })
   }
 
 app.listen(port, () => {
